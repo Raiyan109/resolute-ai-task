d88c02ca889a6799709f41b7d10c010ce41d86f0
Raiyan109 <kraiyan109@gmail.com>
2022-05-29 18:13:03 +0600

    nav, banner, and tool component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './pages/Shared/Header';
+import Home from './pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-
+      <Header></Header>
+      <Home></Home>
     </div>
   );
 }
---
+++ src/pages/Home/Tool.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Tool = () => {
+    return (
+        <div>
+            <h1>this is tool </h1>
+        </div>
+    );
+};
+
+export default Tool;
